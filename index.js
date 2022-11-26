58110bd68c6ad0d86a9b477c18042d1ebe6baf02
philip <philmbugua1@gmail.com>
2022-11-26 09:21:23 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 =62;
+const num2 =1;
+const multiply =(num1*num2);
+
+const random = 7;
+Math.floor ((Math.random() * 17) + 1)
+
+
+const num3= 28;
+const num4= 6;
+const mod =(num3%num4);
+
+const max = 20;
+math.max (20,19,18,17,16);
